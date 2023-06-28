c5d58abfe32e4182a5eb704475e5bb6bd04c2b8c
VictorBauler <victor.bauler@gmail.com>
2023-06-28 11:45:36 -0300

    Add deploy and contract

--- contracts/DexData.sol
+++ contracts/DexData.sol
@@ -3,117 +3,133 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract DexData {
-    struct Candle {
-        uint256 open;
-        uint256 high;
-        uint256 low;
-        uint256 close;
-        uint256 timestamp;
-    }
-
-    struct CandleData {
-        uint256 interval;
-        Candle[] candles;
-    }
-
+    // Struct to represent token data for a specific candle interval
     struct Token {
-        bytes32 ticker;
-        address tokenAddress;
-        CandleData[] candles;
+        bytes32 ticker; // Token ticker symbol
+        address tokenAddress; // Token contract address
+        uint256[] candleOpens; // Array to store open prices for each candle
+        uint256[] candleHighs; // Array to store high prices for each candle
+        uint256[] candleLows; // Array to store low prices for each candle
+        uint256[] candleCloses; // Array to store close prices for each candle
+        uint256[] candleTimestamps; // Array to store timestamps for each candle
     }
 
+    // Struct to represent a decentralized exchange (DEX)
     struct Dex {
-        bytes32 name;
-        address dexAddress;
-        Token[] tokens;
+        bytes32 name; // DEX name
+        address dexAddress; // DEX contract address
+        Token[] tokens; // Array to store tokens listed on the DEX
     }
 
-    Dex[] public dexes;
+    Dex[] public dexes; // Array to store all DEXs
 
+    /**
+     * @dev Function to add a new DEX with associated tokens.
+     * @param _name The name of the DEX.
+     * @param _dexAddress The address of the DEX contract.
+     * @param _tokens An array of Token structs representing the tokens listed on the DEX.
+     */
     function addDex(
         bytes32 _name,
         address _dexAddress,
         Token[] memory _tokens
     ) external {
-        dexes.push(Dex(_name, _dexAddress, _tokens));
+        Dex storage newDex = dexes.push();
+        newDex.name = _name;
+        newDex.dexAddress = _dexAddress;
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            Token storage newToken = newDex.tokens.push();
+            newToken.ticker = _tokens[i].ticker;
+            newToken.tokenAddress = _tokens[i].tokenAddress;
+
+            uint256[] memory opens = new uint256[](
+                _tokens[i].candleOpens.length
+            );
+            uint256[] memory highs = new uint256[](
+                _tokens[i].candleHighs.length
+            );
+            uint256[] memory lows = new uint256[](_tokens[i].candleLows.length);
+            uint256[] memory closes = new uint256[](
+                _tokens[i].candleCloses.length
+            );
+            uint256[] memory timestamps = new uint256[](
+                _tokens[i].candleTimestamps.length
+            );
+
+            for (uint256 j = 0; j < _tokens[i].candleOpens.length; j++) {
+                opens[j] = _tokens[i].candleOpens[j];
+                highs[j] = _tokens[i].candleHighs[j];
+                lows[j] = _tokens[i].candleLows[j];
+                closes[j] = _tokens[i].candleCloses[j];
+                timestamps[j] = _tokens[i].candleTimestamps[j];
+            }
+
+            newToken.candleOpens = opens;
+            newToken.candleHighs = highs;
+            newToken.candleLows = lows;
+            newToken.candleCloses = closes;
+            newToken.candleTimestamps = timestamps;
+        }
     }
 
-    function addCandle(
-        uint256 _dexIndex,
-        uint256 _tokenIndex,
-        uint256 _candleInterval,
-        uint256 _open,
-        uint256 _high,
-        uint256 _low,
-        uint256 _close,
-        uint256 _timestamp
-    ) external {
-        require(_dexIndex < dexes.length, "Invalid Dex index");
-        require(
-            _tokenIndex < dexes[_dexIndex].tokens.length,
-            "Invalid Token index"
-        );
+    /**
+     * @dev Function to get the details of a specific DEX.
+     * @param _dexIndex The index of the DEX in the dexes array.
+     * @return name The name of the DEX.
+     * @return dexAddress The address of the DEX contract.
+     * @return tokens An array of Token structs representing the tokens listed on the DEX.
+     */
 
-        Candle memory newCandle = Candle(
-            _open,
-            _high,
-            _low,
-            _close,
-            _timestamp
-        );
-        dexes[_dexIndex].tokens[_tokenIndex].candles.push(
-            CandleData(_candleInterval, newCandle)
-        );
+    function getDexDetails(
+        uint256 _dexIndex
+    )
+        external
+        view
+        returns (bytes32 name, address dexAddress, Token[] memory tokens)
+    {
+        require(_dexIndex < dexes.length, "Invalid DEX index");
+
+        Dex storage dex = dexes[_dexIndex];
+        return (dex.name, dex.dexAddress, dex.tokens);
     }
 
-    function getCandle(
+    /**
+     * @dev Function to get the candle data for a specific token on a DEX.
+     * @param _dexIndex The index of the DEX in the dexes array.
+     * @param _tokenIndex The index of the token in the tokens array of the DEX.
+     * @return opens An array of open prices for each candle of the token.
+     * @return highs An array of high prices for each candle of the token.
+     * @return lows An array of low prices for each candle of the token.
+     * @return closes An array of close prices for each candle of the token.
+     * @return timestamps An array of timestamps for each candle of the token.
+     */
+    function getTokenCandleData(
         uint256 _dexIndex,
-        uint256 _tokenIndex,
-        uint256 _candleIndex
+        uint256 _tokenIndex
     )
         external
         view
         returns (
-            uint256 open,
-            uint256 high,
-            uint256 low,
-            uint256 close,
-            uint256 timestamp
+            uint256[] memory opens,
+            uint256[] memory highs,
+            uint256[] memory lows,
+            uint256[] memory closes,
+            uint256[] memory timestamps
         )
     {
-        require(_dexIndex < dexes.length, "Invalid Dex index");
+        require(_dexIndex < dexes.length, "Invalid DEX index");
         require(
             _tokenIndex < dexes[_dexIndex].tokens.length,
-            "Invalid Token index"
-        );
-        require(
-            _candleIndex < dexes[_dexIndex].tokens[_tokenIndex].candles.length,
-            "Invalid Candle index"
+            "Invalid token index"
         );
 
-        Candle memory candle = dexes[_dexIndex]
-            .tokens[_tokenIndex]
-            .candles[_candleIndex]
-            .candles[0];
+        Token storage token = dexes[_dexIndex].tokens[_tokenIndex];
         return (
-            candle.open,
-            candle.high,
-            candle.low,
-            candle.close,
-            candle.timestamp
+            token.candleOpens,
+            token.candleHighs,
+            token.candleLows,
+            token.candleCloses,
+            token.candleTimestamps
         );
-    }
-
-    function getCandleCount(
-        uint256 _dexIndex,
-        uint256 _tokenIndex
-    ) external view returns (uint256) {
-        require(_dexIndex < dexes.length, "Invalid Dex index");
-        require(
-            _tokenIndex < dexes[_dexIndex].tokens.length,
-            "Invalid Token index"
-        );
-
-        return dexes[_dexIndex].tokens[_tokenIndex].candles.length;
     }
 }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,2 +1,60 @@
+from brownie import accounts, config, DexData, web3
+
+
 def main():
-    print("Deploying...")
+    # Load the deployer account
+    deployer = accounts[0]
+
+    # Deploy the contract
+    dex_data = DexData.deploy({"from": deployer})
+
+    # Print the contract address
+    print("Contract deployed at:", dex_data.address)
+
+    # Convert dex_name to bytes32
+    dex_name_bytes32 = web3.toBytes(text="MyDEX")  # Replace "MyDEX" with your desired DEX name
+
+    # Add DEX details
+    dex_address = "0x1234567890123456789012345678901234567890"  # Replace with your actual DEX address
+    tokens = [
+        (
+            web3.toBytes(text="TKN1"),
+            "0x1234567890123456789012345678901234567891",  # Replace with your token address
+            [100, 150, 200],  # Example candle opens
+            [120, 180, 220],  # Example candle highs
+            [80, 130, 180],  # Example candle lows
+            [110, 160, 210],  # Example candle closes
+            [1630000000, 1630003600, 1630007200]  # Example candle timestamps
+        ),
+        (
+            web3.toBytes(text="TKN2"),
+            "0x1234567890123456789012345678901234567892",  # Replace with your token address
+            [100, 150, 200],  # Example candle opens
+            [120, 180, 220],  # Example candle highs
+            [80, 130, 180],  # Example candle lows
+            [110, 160, 210],  # Example candle closes
+            [1630000000, 1630086400, 1630172800]  # Example candle timestamps
+        ),
+    ]
+    dex_data.addDex(dex_name_bytes32, dex_address, tokens)
+
+    # Retrieve and print DEX details
+    dex_index = 0  # Replace with the desired DEX index
+    name, address, listed_tokens = dex_data.getDexDetails(dex_index)
+    print("DEX Details:")
+    print("Name:", web3.toText(name))
+    print("Address:", address)
+    print("Listed Tokens:")
+    for token in listed_tokens:
+        print("     Token Name:", web3.toText(token[0]))
+        print("     Token Address:", token[1])
+        print("     Token Candle Data:")
+        print("     Opens:", token[2])
+        print("     Highs:", token[3])
+        print("     Lows:", token[4])
+        print("     Closes:", token[5])
+        print("     Timestamps:", token[6])
+        print("     --------------------")
+
+if __name__ == "__main__":
+    main()
